4de0b53bdb7706489a7933911d7f83898a0493f3
ShermanWoo <sherman@ShermanWoos-MacBook-Air.local>
2024-10-31 16:31:13 +0000

    Updated print statement

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import datetime
 print(f"\nthe time is {datetime.datetime.now()}\n")
 
+import platform
+print(f"\nthis is python version {platform.python_version()}\n")
+
+import sys
+print(f"\nthe file is located in {sys.executable}\n")
+
+
+
+
+
+
+
+
+
+
